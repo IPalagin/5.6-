bdea1f7601d8155c77502e602af84668c7700124
Palagin Igor <igor_palagin.56rus@mail.ru>
2023-05-03 19:06:29 +0300

    Доработка итоговой 5.6

--- 5.6 Итоговый проект/Program.cs
+++ 5.6 Итоговый проект/Program.cs
@@ -4,15 +4,13 @@ internal class Program
 {
     private static void Main()
     {
-        ReturnCort();
+        byte petNumb = 0; byte colorNumb = 0;
+        ReturnCort(ref petNumb, ref colorNumb);
+        ShowData(anketa);
     }
-    static void ReturnCort() 
+    static (string Name, string Famaly, byte Age, bool isPets, string[] petArr, string[] colorArr) anketa;
+    static void ReturnCort(ref byte petNumb, ref byte colorNumb)
     {
-        (string Name, string Famaly, byte Age, bool isPets, byte petNum, byte colorNum) anketa;
-        byte petNumb;
-        byte colorNumb;
-        byte ageUser;
-
         Console.WriteLine("Введите имя пользователя");
         anketa.Name = Console.ReadLine();
 
@@ -20,7 +18,6 @@ internal class Program
         anketa.Famaly = Console.ReadLine();
 
         anketa.Age = CheckInputByte("Введите возраст пользователя", 100);
-
         Console.WriteLine("Есть ли у вас питомец ? (Ответьте Да/Нет)");
         var result = Console.ReadLine();
         while (result != "Да" && result != "Нет")
@@ -28,79 +25,66 @@ internal class Program
             Console.WriteLine("Некорректный ответ, пожалуйста, ответьте Да или Нет");
             result = Console.ReadLine();
         }
-
         if (result == "Да")
         {
-            anketa.isPets = true;
+            anketa.isPets = true; 
             petNumb = CheckInputByte("Укажите количество питомцев", 10);
             while (petNumb == 0)
             {
                 Console.WriteLine("Количество питомцев не может быть равно 0");
                 petNumb = CheckInputByte("Укажите количество питомцев", 10);
             }
-            anketa.petNum = petNumb;
-            NumPetsName(ref petNumb);
+            anketa.petArr = NumPetsName(ref petNumb);
         }
-        else 
+        else
         {
             anketa.isPets = false;
         }
-
         colorNumb = CheckInputByte("Укажите количество ваших любимых цветов", 10);
-        anketa.colorNum = colorNumb;
-        NumColor(ref colorNumb);
+        anketa.colorArr = NumColor(ref colorNumb);
     }
-
     static string[] NumPetsName(ref byte petNumb)
     {
         string[] petName = new string[petNumb];
-
-        Console.WriteLine("Назовите кличку(ки) своего(их) питомцев");
+        Console.WriteLine("Назовите кличку(ки) своего(их) питомцев"); 
         for (int i = 0; i < petNumb; i++)
         {
             petName[i] = Console.ReadLine();
         }
-        return petName;   
+        return petName;
     }
-
     static string[] NumColor(ref byte colorNumb)
     {
         string[] color = new string[colorNumb];
-
-        Console.WriteLine("Назовите ваши любимые цвета");
-        for (int j = 0;j < colorNumb; j++)
+        Console.WriteLine("Назовите ваши любимые цвета"); 
+        for (int j = 0; j < colorNumb; j++)
         {
             color[j] = Console.ReadLine();
         }
         return color;
     }
-
     static byte CheckInputByte(string message, byte maxAllowedValue)
     {
-        byte result;
-        bool isValidInput;
+        byte result; bool isValidInput;
         do
         {
-            Console.WriteLine(message);
+            Console.WriteLine(message); 
             isValidInput = byte.TryParse(Console.ReadLine(), out result) && result <= maxAllowedValue;
             if (!isValidInput)
             {
                 Console.WriteLine($"Некорректное значение. Введите число от 0 до {maxAllowedValue}.");
             }
-        } while (!isValidInput);
+        } while (!isValidInput); 
         return result;
     }
-
-    static void ShowData((string Name, string Famaly, byte Age, bool isPets, byte petNum, byte colorNum) anketa, string[] petName, string[] color)
+    static void ShowData((string Name, string Famaly, byte Age, bool isPets, string[] petArr, string[] colorArr) anketa)
     {
         Console.WriteLine($"Имя: {anketa.Name}");
         Console.WriteLine($"Фамилия: {anketa.Famaly}");
-        Console.WriteLine($"Возраст: {anketa.Age}");
-        if (anketa.isPets)
+        Console.WriteLine($"Возраст: {anketa.Age}"); if (anketa.isPets)
         {
-            Console.WriteLine($"Количество питомцев: {anketa.petNum}");
-            Console.WriteLine("Клички питомцев:");
-            foreach (string pet in petName)
+            Console.WriteLine($"Количество питомцев: {anketa.petArr.Length}");
+            Console.WriteLine("Клички питомцев:"); foreach (string pet in anketa.petArr)
             {
                 Console.WriteLine(pet);
             }
@@ -109,9 +93,9 @@ internal class Program
         {
             Console.WriteLine("У пользователя нет питомцев");
         }
-        Console.WriteLine($"Количество любимых цветов: {anketa.colorNum}");
+        Console.WriteLine($"Количество любимых цветов: {anketa.colorArr.Length}"); 
         Console.WriteLine("Любимые цвета:");
-        foreach (string col in color)
+        foreach (string col in anketa.colorArr)
         {
             Console.WriteLine(col);
         }
